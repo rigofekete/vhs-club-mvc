50a01ddedb0b7ed6830c599990146b59e991827a
rigofekete <fabricecosteira@gmail.com>
2026-02-06 20:03:32 +0100

    created config/config.go to manage application configuration

---
+++ config/config.go
@@ -0,0 +1,41 @@
+package config
+
+import (
+	// driver import for sqlc, only imported for its side effects (DB communication)
+	"database/sql"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/rigofekete/vhs-club-mvc/internal/database"
+)
+
+type Config struct {
+	DB *database.Queries
+}
+
+var AppConfig *Config
+
+func Load() {
+	_ = godotenv.Load()
+
+	AppConfig = &Config{
+		DB: getEnv("DB_URL"),
+	}
+}
+
+// Refactor this later when we will need to fill more config fields
+func getEnv(dbString string) *database.Queries {
+	dbURL := os.Getenv(dbString)
+	if dbURL == "" {
+		log.Fatal("DB_URL must be set")
+	}
+	dbConn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("error opening databse: %v", err)
+	}
+	dbQueries := database.New(dbConn)
+
+	return dbQueries
+}
