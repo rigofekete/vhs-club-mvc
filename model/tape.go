4ad98e34ebd2b0795d90ade1bdba5bc0ee1c58b3
rigofekete <fabricecosteira@gmail.com>
2026-02-07 00:16:54 +0100

    updated tape model.

--- model/tape.go
+++ model/tape.go
@@ -1,10 +1,24 @@
 package model
 
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
 type Tape struct {
-	ID       string  `json:"id"`
-	Title    string  `json:"title"`
-	Director string  `json:"director"`
-	Genre    string  `json:"genre"`
-	Quantity int     `json:"quantity"`
-	Price    float64 `json:"price"`
+	// ID        string    `json:"id"`
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Title     string    `json:"title"`
+	Director  string    `json:"director"`
+	Genre     string    `json:"genre"`
+	Quantity  int       `json:"quantity"`
+	// UserID    uuid.UUID `json:"user_id"`
+	Price float64 `json:"price"`
 }
+
+// type User struct {
+// 	ID string `json:"id"`
+// }
