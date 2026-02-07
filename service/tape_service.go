2c6251a2d0655d2a387b7c084cd77efa9b91d80c
rigofekete <fabricecosteira@gmail.com>
2026-02-07 18:17:05 +0100

    refactored Update method to receive model.UpdatedTape, only the fields in this model are allowed to be updated. This type also as pointers to each field instead make validation more easy. validateUpdatedTape helper function created to validate the incoming partial update before dispatching it to the repo layer.

--- service/tape_service.go
+++ service/tape_service.go
@@ -1,7 +1,11 @@
 package service
 
 import (
+	"database/sql"
+	"log"
+
 	"github.com/google/uuid"
+	"github.com/rigofekete/vhs-club-mvc/internal/database"
 	"github.com/rigofekete/vhs-club-mvc/model"
 	"github.com/rigofekete/vhs-club-mvc/repository"
 )
@@ -10,7 +14,7 @@ type TapeService interface {
 	Create(model.Tape) *model.Tape
 	List() []model.Tape
 	GetTapeByID(id string) (*model.Tape, bool)
-	Update(id string, updated model.Tape) (*model.Tape, bool)
+	Update(id string, updated model.UpdatedTape) (*model.Tape, bool)
 	Delete(id string) bool
 }
 
@@ -23,6 +27,59 @@ func NewTapeService(r repository.TapeRepository) TapeService {
 		repo: r,
 	}
 }
+
+// Helper for the Update method
+func validateUpdatedTape(id uuid.UUID, updated model.UpdatedTape) *database.UpdateTapeParams {
+	dbUpdatedParams := database.UpdateTapeParams{
+		ID: id,
+	}
+
+	changes := false
+
+	if updated.Title != nil {
+		dbUpdatedParams.Title = sql.NullString{String: *updated.Title, Valid: true}
+		changes = true
+	} else {
+		dbUpdatedParams.Title = sql.NullString{Valid: false}
+	}
+
+	if updated.Director != nil {
+		dbUpdatedParams.Director = sql.NullString{String: *updated.Director, Valid: true}
+		changes = true
+	} else {
+		dbUpdatedParams.Director = sql.NullString{Valid: false}
+	}
+
+	if updated.Genre != nil {
+		dbUpdatedParams.Genre = sql.NullString{String: *updated.Genre, Valid: true}
+		changes = true
+	} else {
+		dbUpdatedParams.Genre = sql.NullString{Valid: false}
+	}
+
+	if updated.Quantity != nil {
+		dbUpdatedParams.Quantity = sql.NullInt32{Int32: *updated.Quantity, Valid: true}
+		changes = true
+	} else {
+		dbUpdatedParams.Quantity = sql.NullInt32{Valid: false}
+	}
+
+	if updated.Price != nil {
+		dbUpdatedParams.Price = sql.NullFloat64{Float64: *updated.Price, Valid: true}
+		changes = true
+	} else {
+		dbUpdatedParams.Price = sql.NullFloat64{Valid: false}
+	}
+
+	if !changes {
+		return nil
+	}
+
+	return &dbUpdatedParams
+}
+
+// TapeService Methods
+//////////////////////
 
 func (s *tapeService) Create(tape model.Tape) *model.Tape {
 	// if !validTape(tape) {
@@ -47,25 +104,32 @@ func (s *tapeService) List() []model.Tape {
 func (s *tapeService) GetTapeByID(id string) (*model.Tape, bool) {
 	tapeID, err := uuid.Parse(id)
 	if err != nil {
-		// TODO: Check if it is need to return an error from this layer to the handler
+		log.Printf("error parsing id string to uuid: %v", err)
 		return nil, false
 	}
 	return s.repo.FindByID(tapeID)
 }
 
-func (s *tapeService) Update(id string, updated model.Tape) (*model.Tape, bool) {
+func (s *tapeService) Update(id string, updated model.UpdatedTape) (*model.Tape, bool) {
 	tapeID, err := uuid.Parse(id)
 	if err != nil {
-		// TODO: Check if it is need to return an error from this layer to the handler
+		log.Printf("error parsing id string to uuid: %v", err)
 		return nil, false
 	}
-	return s.repo.Update(tapeID, updated)
+
+	dbUpdatedParams := validateUpdatedTape(tapeID, updated)
+	if dbUpdatedParams == nil {
+		log.Print("invalid updated tape")
+		return nil, false
+	}
+
+	return s.repo.Update(tapeID, *dbUpdatedParams)
 }
 
 func (s *tapeService) Delete(id string) bool {
 	tapeID, err := uuid.Parse(id)
 	if err != nil {
-		// TODO: Check if it is need to return an error from this layer to the handler
+		log.Printf("error parsing id string to uuid: %v", err)
 		return false
 	}
 	return s.repo.Delete(tapeID)
