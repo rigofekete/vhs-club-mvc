e59cf742c85ae923cdde5ca6bc179e939b3ad5a8
rigofekete <fabricecosteira@gmail.com>
2026-02-07 12:03:03 +0100

    removed user type for now.

--- model/tape.go
+++ model/tape.go
@@ -7,7 +7,6 @@ import (
 )
 
 type Tape struct {
-	// ID        string    `json:"id"`
 	ID        uuid.UUID `json:"id"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
@@ -15,10 +14,5 @@ type Tape struct {
 	Director  string    `json:"director"`
 	Genre     string    `json:"genre"`
 	Quantity  int       `json:"quantity"`
-	// UserID    uuid.UUID `json:"user_id"`
-	Price float64 `json:"price"`
+	Price     float64   `json:"price"`
 }
-
-// type User struct {
-// 	ID string `json:"id"`
-// }
