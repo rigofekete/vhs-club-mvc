422b2c4e92ce8340d64a3f66e20947fc67e60692
rigofekete <fabricecosteira@gmail.com>
2026-02-12 17:53:17 +0100

    Generation of default values is now done at the table creation.

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -1,9 +1,6 @@
 -- name: CreateUser :one
-INSERT INTO users(id, created_at, updated_at, name, email)
+INSERT INTO users(name, email)
 VALUES (
-  gen_random_uuid(),
-  NOW(),
-  NOW(),
   $1,
   $2
 )
