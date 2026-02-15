1950f819442df16738fb82ed71d51d4f1b74dff6
rigofekete <fabricecosteira@gmail.com>
2026-02-15 11:06:17 +0100

    methods refactored to know receive the domain model as a pointer.

--- service/user_service.go
+++ service/user_service.go
@@ -1,23 +1,18 @@
 package service
 
 import (
-	"github.com/rigofekete/vhs-club-mvc/internal/apperror"
 	"github.com/rigofekete/vhs-club-mvc/model"
 	"github.com/rigofekete/vhs-club-mvc/repository"
 )
 
 type UserService interface {
-	CreateUser(model.User) (*model.User, error)
-	ListUsers() ([]model.User, error)
+	CreateUser(*model.User) (*model.User, error)
+	ListUsers() ([]*model.User, error)
 	DeleteAllUsers() error
 }
 
 type userService struct {
 	repo repository.UserRepository
-}
-
-func validUserFields(user model.User) bool {
-	return user.Name != "" && user.Email != ""
 }
 
 func NewUserService(r repository.UserRepository) UserService {
@@ -26,14 +21,11 @@ func NewUserService(r repository.UserRepository) UserService {
 	}
 }
 
-func (s *userService) CreateUser(user model.User) (*model.User, error) {
-	if !validUserFields(user) {
-		return nil, apperror.ErrUserValidation
-	}
+func (s *userService) CreateUser(user *model.User) (*model.User, error) {
 	return s.repo.Save(user)
 }
 
-func (s *userService) ListUsers() ([]model.User, error) {
+func (s *userService) ListUsers() ([]*model.User, error) {
 	return s.repo.FindAll()
 }
 
