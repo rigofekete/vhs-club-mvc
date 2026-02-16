e6fe8dce815fbb6c837c0aac8c95e6868f39a11a
rigofekete <fabricecosteira@gmail.com>
2026-02-17 00:44:01 +0100

    Added GetAllActiveRentals, GetActiveRentalCountByTape query methods. Renamed GetActiveRentalByID

--- sql/queries/rentals.sql
+++ sql/queries/rentals.sql
@@ -11,7 +11,7 @@ UPDATE rentals
 SET returned_at = $2
 WHERE id = $1;
 
--- name: GetActiveRentalByID :many
+-- name: GetActiveRentalByUserID :many
 SELECT * FROM rentals
 -- NULL is not a value so only IS keyword works
 WHERE user_id = $1 AND returned_at IS NULL;
@@ -19,3 +19,12 @@ WHERE user_id = $1 AND returned_at IS NULL;
 -- name: GetActiveRentalbyTape :many
 SELECT * FROM rentals
 WHERE tape_id = $1 AND returned_at IS NULL;
+
+-- name: GetActiveRentalCountByTape :one
+SELECT COUNT(*) FROM rentals
+WHERE tape_id = $1 AND returned_at IS NULL;
+
+-- name: GetAllActiveRentals :many
+SELECT * FROM rentals
+WHERE returned_at IS NULL
+ORDER BY created_at ASC;
