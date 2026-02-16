580df5ebebfeb7661a74d9f00a968ee8d8d1fee5
rigofekete <fabricecosteira@gmail.com>
2026-02-16 11:38:56 +0100

    Removed json tags from tape domain model. These will be only present in the request/response (dtos)

--- model/tape.go
+++ model/tape.go
@@ -7,21 +7,22 @@ import (
 )
 
 type Tape struct {
-	ID        int32     `json:"id"`
-	PublicID  uuid.UUID `json:"public_id"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-	Title     string    `json:"title"`
-	Director  string    `json:"director"`
-	Genre     string    `json:"genre"`
-	Quantity  int32     `json:"quantity"`
-	Price     float64   `json:"price"`
+	ID        int32
+	PublicID  uuid.UUID
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	Title     string
+	Director  string
+	Genre     string
+	Quantity  int32
+	Price     float64
 }
 
-type UpdatedTape struct {
-	Title    *string  `json:"title"`
-	Director *string  `json:"director"`
-	Genre    *string  `json:"genre"`
-	Quantity *int32   `json:"quantity"`
-	Price    *float64 `json:"price"`
+type UpdateTape struct {
+	ID       int32
+	Title    *string
+	Director *string
+	Genre    *string
+	Quantity *int32
+	Price    *float64
 }
