39392f782f3f74b9fd0f8ba072400de793b08537
rigofekete <fabricecosteira@gmail.com>
2026-02-17 12:26:46 +0100

    rental model now includes tape title and username for more clarity.

--- model/rental.go
+++ model/rental.go
@@ -8,11 +8,13 @@ import (
 )
 
 type Rental struct {
-	ID         int32        `json:"id"`
-	PublicID   uuid.UUID    `json:"public_id"`
-	CreatedAt  time.Time    `json:"created_at"`
-	UserID     int32        `json:"user_id"`
-	TapeID     int32        `json:"tape_id"`
-	RentedAt   time.Time    `json:"rented_at"`
-	ReturnedAt sql.NullTime `json:"returned_at"`
+	ID         int32
+	PublicID   uuid.UUID
+	CreatedAt  time.Time
+	UserID     int32
+	TapeID     int32
+	TapeTitle  string
+	Username   string
+	RentedAt   time.Time
+	ReturnedAt sql.NullTime
 }
