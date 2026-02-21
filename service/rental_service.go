3202fa64b2ab10570dcfb925c9b6e0f2d7bab2e0
rigofekete <fabricecosteira@gmail.com>
2026-02-21 14:03:04 +0100

    Removed error wrapping from the rental service layer. Appropriate apperror error will be thrown be the repo layer. Returned counts are now pointers, dereferencing them for the business logic checks.

--- service/rental_service.go
+++ service/rental_service.go
@@ -2,7 +2,6 @@ package service
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/google/uuid"
 	"github.com/rigofekete/vhs-club-mvc/internal/apperror"
@@ -45,21 +44,12 @@ func (s *rentalService) RentTape(ctx context.Context, tapePublicID, userPublicID
 
 	tape, err := s.tapeRepo.GetByPublicID(ctx, tapeUUID)
 	if err != nil {
-		return nil, fmt.Errorf("could not find tape from given public id: %w: %v", apperror.ErrTapeNotFound, err)
+		return nil, err
 	}
 
 	user, err := s.userRepo.GetByPublicID(ctx, userUUID)
 	if err != nil {
-		return nil, fmt.Errorf("could not find user from given public id: %w: %v", apperror.ErrUserNotFound, err)
-	}
-
-	countByUser, err := s.rentalRepo.GetActiveRentCountByUser(ctx, user.ID)
-	if err != nil {
 		return nil, err
-	}
-
-	if int32(countByUser) >= maxRentalsPerUser {
-		return nil, apperror.ErrMaxRentalsPerUser
 	}
 
 	countByTape, err := s.rentalRepo.GetActiveRentCountByTape(ctx, tape.ID)
@@ -67,11 +57,20 @@ func (s *rentalService) RentTape(ctx context.Context, tapePublicID, userPublicID
 		return nil, err
 	}
 
-	if int32(countByTape) >= tape.Quantity {
+	if int32(*countByTape) >= tape.Quantity {
 		return nil, apperror.ErrTapeUnavailable
 	}
 
-	return s.rentalRepo.Save(tape.ID, user.ID)
+	countByUser, err := s.rentalRepo.GetActiveRentCountByUser(ctx, user.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	if int32(*countByUser) >= maxRentalsPerUser {
+		return nil, apperror.ErrMaxRentalsPerUser
+	}
+
+	return s.rentalRepo.Save(ctx, tape.ID, user.ID)
 }
 
 func (s *rentalService) GetAllActiveRentals(ctx context.Context) ([]*model.Rental, error) {
