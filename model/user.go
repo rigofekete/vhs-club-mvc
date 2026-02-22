32f209ea16d9b37e485d43ea5180f23f2b387d1a
rigofekete <fabricecosteira@gmail.com>
2026-02-22 23:35:59 +0100

    User domain model now holds Password and HashPassword.

--- model/user.go
+++ model/user.go
@@ -7,11 +7,12 @@ import (
 )
 
 type User struct {
-	ID        int32
-	PublicID  uuid.UUID
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	Username  string
-	Email     string
-	Password  string
+	ID             int32
+	PublicID       uuid.UUID
+	CreatedAt      time.Time
+	UpdatedAt      time.Time
+	Username       string
+	Email          string
+	Password       string
+	HashedPassword string
 }
