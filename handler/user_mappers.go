58294e473b3229d0d61a4f7680ed4002692593e9
rigofekete <fabricecosteira@gmail.com>
2026-02-22 18:30:50 +0100

    Response returned objects are no longer pointers.

--- handler/user_mappers.go
+++ handler/user_mappers.go
@@ -9,16 +9,24 @@ func (r *CreateUserRequest) ToModel() *model.User {
 	}
 }
 
-func UserSingleResponse(user *model.User) *UserResponse {
-	return &UserResponse{
+func (r *CreateUserBatchRequest) ToModels() []*model.User {
+	users := make([]*model.User, 0, len(r.Users))
+	for _, u := range r.Users {
+		users = append(users, u.ToModel())
+	}
+	return users
+}
+
+func UserSingleResponse(user *model.User) UserResponse {
+	return UserResponse{
 		PublicID: user.PublicID,
 		Username: user.Username,
 		Email:    user.Email,
 	}
 }
 
-func UserListResponse(users []*model.User) []*UserResponse {
-	userList := make([]*UserResponse, len(users))
+func UserListResponse(users []*model.User) []UserResponse {
+	userList := make([]UserResponse, len(users))
 	for i, user := range users {
 		userList[i] = UserSingleResponse(user)
 	}
