deaec9c05c38a1598df0497ef729e96b383b9187
rigofekete <fabricecosteira@gmail.com>
2026-02-22 20:27:49 +0100

    added hashed_password to the User table definition.

--- sql/schema/002_users.sql
+++ sql/schema/002_users.sql
@@ -5,7 +5,8 @@ CREATE TABLE users(
   created_at  TIMESTAMP NOT NULL DEFAULT NOW(),
   updated_at  TIMESTAMP NOT NULL DEFAULT NOW(),
   username    TEXT  NOT NULL UNIQUE,
-  email       TEXT  NOT NULL UNIQUE
+  email       TEXT  NOT NULL UNIQUE,
+  hashed_password TEXT NOT NULL
 );
 
 -- +goose Down
