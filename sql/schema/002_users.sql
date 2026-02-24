91a5aa677f863f2ff6f041de0ca4b0b47ecf1fb9
rigofekete <fabricecosteira@gmail.com>
2026-02-24 09:54:20 +0100

    Added role field to the Users table structure.

--- sql/schema/002_users.sql
+++ sql/schema/002_users.sql
@@ -1,12 +1,13 @@
 -- +goose Up
 CREATE TABLE users(
-  id          INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  public_id   UUID UNIQUE NOT NULL DEFAULT gen_random_uuid(),
-  created_at  TIMESTAMP NOT NULL DEFAULT NOW(),
-  updated_at  TIMESTAMP NOT NULL DEFAULT NOW(),
-  username    TEXT  NOT NULL UNIQUE,
-  email       TEXT  NOT NULL UNIQUE,
-  hashed_password TEXT NOT NULL
+  id               INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  public_id        UUID UNIQUE NOT NULL DEFAULT gen_random_uuid(),
+  created_at       TIMESTAMP NOT NULL DEFAULT NOW(),
+  updated_at       TIMESTAMP NOT NULL DEFAULT NOW(),
+  username         TEXT  NOT NULL UNIQUE,
+  email            TEXT  NOT NULL UNIQUE,
+  role             TEXT NOT NULL DEFAULT 'user',
+  hashed_password  TEXT NOT NULL
 );
 
 -- +goose Down
