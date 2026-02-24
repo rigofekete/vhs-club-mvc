71262f925734337d47857862c8127231f9e629aa
rigofekete <fabricecosteira@gmail.com>
2026-02-24 23:01:06 +0100

    App.jsx now imports routing exports to define the route navigation plan of our pages.

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,79 +1,17 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import vhsLogo from './assets/vhsLogo.svg'
-// import Header from './components/Header'
-import InputField from './components/InputField'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import Login from './pages/Login'
+import Dashboard from './pages/Dashboard'
 
 function App() {
-  const [logged, setLogin] = useState(false);
-  const [users, setUsers] = useState([]);
-  const [username, setUsername] = useState('');
-  const [error, setError] = useState('');
-
-
-  const inputFields = [
-    { className: "Username", type: "text", label: "Username" },
-    { className: "Password", type: "text", label: "Password" },
-  ];
-
-  const handleChange = (event) => {
-    setUsername(event.target.value);
-  };
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('http://localhost:8080/users');
-        const jsonData = await response.json();
-        setUsers(jsonData);
-      } catch (err) { console.error('Failed to fetch users:', err); }
-    };
-    fetchData();
-  }, []);
-
-
-  const handleLogin = (event) => {
-    event.preventDefault();
-
-    const userExists = users.some((user) => user.username === username);
-
-    console.log('Users fetched', users);
-
-    if (userExists) {
-      setLogin(true);
-      console.log("Login complete!")
-    } else {
-      setError('Username not found. Please try again.');
-    }
-  };
-
   return (
-    <>
-      <div className="App">
-        < img src={vhsLogo}
-          className={logged ? "logo-blink" : "logo}"}
-          alt="Vhs Logo"
-        />
-        {logged ? (
-          <p className="welcome-text"> Welcome, {username}</p>
-        ) : (
-          <form onSubmit={handleLogin} onChange={handleChange}>
-            {inputFields.map((inputField, index) => (
-              <InputField
-                key={index}
-                className={inputField.className}
-                type={inputField.type}
-                label={inputField.label}
-              />
-            ))}
-            <button className="button" type="submit">Login</button>
-            {error && <p className="error-message">{error}</p>}
-          </form>
-        )}
-      </div>
-    </>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </div>
   )
 }
 
 export default App
-
