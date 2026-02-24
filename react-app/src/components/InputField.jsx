44a9e514b3a093525633f944f604308efb3c2fe7
rigofekete <fabricecosteira@gmail.com>
2026-02-24 23:04:11 +0100

    Receiving onChange prop from Login.jsx, which will be called in the input element, on change trigger.

--- react-app/src/components/InputField.jsx
+++ react-app/src/components/InputField.jsx
@@ -3,13 +3,15 @@ function InputField(props) {
   const className = props.className;
   const type = props.type;
   const label = props.label;
+  //TODO: Any way to get diagnostic warnings in jsx? for example if I type Onchange here, there is no lsp warning for undefined variable.
+  const onChange = props.onChange;
 
   return (
     <div className={`input-wrapper ${className}`}>
       <label>
         {label}
       </label>
-      <input type={type} />
+      <input type={type} onChange={onChange} />
     </div>
   );
 }
