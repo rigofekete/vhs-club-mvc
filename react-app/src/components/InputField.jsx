65330ecb8ef3444beaf2be8d343059c754aed137
rigofekete <fabricecosteira@gmail.com>
2026-03-03 12:12:04 +0100

    refactored component function return. No label element will be rendered now. Field now shows the label as a placeholder until user starts typing.

--- react-app/src/components/InputField.jsx
+++ react-app/src/components/InputField.jsx
@@ -3,15 +3,10 @@ function InputField(props) {
   const className = props.className;
   const type = props.type;
   const label = props.label;
-  //TODO: Any way to get diagnostic warnings in jsx? for example if I type Onchange here, there is no lsp warning for undefined variable.
   const onChange = props.onChange;
 
   return (
     <div className={`input-wrapper ${className}`}>
-      <label>
-        {/* {label} */}
-      </label>
-      {/* <input type={type} onChange={onChange} /> */}
       <input type={type} onChange={onChange} placeholder={label} />
     </div>
   );
