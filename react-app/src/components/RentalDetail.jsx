68545b2b381c8cebb91782f0e1671de943bee9cb
rigofekete <fabricecosteira@gmail.com>
2026-03-05 15:26:06 +0100

    created RentalDetail component, acting similarly to TapeDetail but with option to return the rented tape.

---
+++ react-app/src/components/RentalDetail.jsx
@@ -0,0 +1,21 @@
+
+function RentalDetail({ rental, error, onBack, onReturnRent }) {
+  return (
+    <div className="tape-detail">
+      <img className="tape-detail-cover"
+        src={`/tapes/${rental.tape_title}.png`}
+      />
+      <h3>{rental.tape_title}</h3>
+      {/* <p>{rental.director}</p> */}
+      <button className="panel-button" onClick={onReturnRent}>
+        Return Tape
+      </button>
+      <button className="panel-button" onClick={onBack}>
+        Back
+      </button>
+      {error && <h1 className="error-message">{error}</h1>}
+    </div>
+  );
+}
+
+export default RentalDetail;
