7bee88e92d82db57eb13f1a81084420518bc73a8
rigofekete <fabricecosteira@gmail.com>
2026-03-05 15:26:47 +0100

    moved error paragraph element to the bottom of the div.

--- react-app/src/components/TapeDetail.jsx
+++ react-app/src/components/TapeDetail.jsx
@@ -6,13 +6,13 @@ function TapeDetail({ tape, error, onBack, onRent }) {
       />
       <h3>{tape.title}</h3>
       <p>{tape.director}</p>
-      {error && <p className="error-message">{error}</p>}
       <button className="panel-button" onClick={onRent}>
         Rent
       </button>
       <button className="panel-button" onClick={onBack}>
         Back
       </button>
+      {error && <h1 className="error-message">{error}</h1>}
     </div>
   );
 }
