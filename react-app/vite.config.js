1359e77680927187b96e5e9c674f71c43d180852
rigofekete <fabricecosteira@gmail.com>
2026-03-24 22:22:39 +0100

    Added test configuration to vite.config.js.  Created setupTests.js in react-app/src.

--- react-app/vite.config.js
+++ react-app/vite.config.js
@@ -11,4 +11,12 @@ export default defineConfig({
     },
   },
   plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/setupTests.js',
+    coverage: {
+      include: ['src/**/*.jsx'],
+    },
+  },
 })
